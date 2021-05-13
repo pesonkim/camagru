1476509080e910ff41408d38749df08595be8c42
kpesonen <pesonkim@gmail.com>
2021-05-13 20:19:24 +0300

    upload file preview work

--- app/assets/js/upload.js
+++ app/assets/js/upload.js
@@ -1,32 +1,126 @@
 var uploadWrapper = document.getElementById('uploadWrapper');
+var uploadPreview = document.getElementById('uploadPreview');
+var uploadForm = document.getElementById('uploadForm');
+var uploadInput = document.getElementById('fileUpload');
+var webcamInput = document.getElementById('webcamUpload');
+
+var menu1 = document.getElementById('menu1');
 var uploadBtn = document.getElementById('manualUpload');
-var input = document.getElementById('fileUpload');
-var fileName = document.createElement('span');
+var webcamBtn = document.getElementById('webCam');
 
-function getFileNameWithExt(event) {
+var menu2 = document.getElementById('menu2');
+var webcamCancel = document.getElementById('camCancel');
+var webcamCapture = document.getElementById('camCapture');
 
-    if (!event || !event.target || !event.target.files || event.target.files.length === 0) {
-      return;
-    }
-  
-    const name = event.target.files[0].name;
-    const lastDot = name.lastIndexOf('.');
-  
-    const fileName = name.substring(0, lastDot);
-    const ext = name.substring(lastDot + 1);
-  
-    console.log(fileName+'.'+ext);    
+var menu3 = document.getElementById('menu3');
+var fileCancel = document.getElementById('imgCancel');
+var fileContinue = document.getElementById('imgContinue');
+
+var uploadFile = undefined;
+var fileName = document.getElementById('fileName');
+
+const videoPlayer = document.getElementById('videoPlayer');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+function getFileName() {
+    uploadFile = uploadInput.value;
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-    uploadWrapper.querySelector('.uploadMedia').appendChild(fileName);
-});
+function displayFileName() {
+    if (uploadFile == undefined || uploadFile == '') {
+        fileName.innerHTML = 'No file selected.'
+    }
+    else {
+        fileName.innerHTML = uploadFile;
+    }
+}
 
+
+
+
+
+var video = document.querySelector("#videoElement");
+
+webcamCancel.addEventListener('click', function() {
+    videoPlayer.srcObject.getVideoTracks().forEach(track => track.stop());
+    fileName.style.display = 'block';
+    videoPlayer.style.display = 'none';
+    menu1.style.display = 'flex';
+    menu2.style.display = 'none';
+    menu3.style.display = 'none';
+})
+
+webcamCapture.addEventListener('click', function() {
+    canvas.width = videoPlayer.srcObject.getVideoTracks()[0].getSettings().width;
+    canvas.height = videoPlayer.srcObject.getVideoTracks()[0].getSettings().height;
+
+    context.drawImage(videoPlayer, 0, 0);
+    videoPlayer.srcObject.getVideoTracks().forEach(track => track.stop());
+
+    var capture = canvas.toDataURL('image/png');
+    var canvasCapture = document.getElementById('canvasCapture');
+    canvasCapture.src = capture;
+
+    videoPlayer.style.display = 'none';
+    canvasCapture.style.display = 'block';
+    menu1.style.display = 'none';
+    menu2.style.display = 'none';
+    menu3.style.display = 'flex';
+})
+
+webcamBtn.addEventListener('click', function() {
+    if (videoPlayer.style.display == 'none') {
+        fileName.style.display = 'none';
+        videoPlayer.style.display = 'block';
+        const constraints = {
+            video: true,
+        };
+        navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+            videoPlayer.srcObject = stream;
+        });
+        menu1.style.display = 'none';
+        menu2.style.display = 'flex';
+        menu3.style.display = 'none';
+    }
+})
+
+uploadForm.addEventListener('change', function() {
+    getFileName();
+    displayFileName();
+    if (uploadFile !== '') {
+        menu1.style.display = 'none';
+        menu3.style.display = 'flex';
+    }
+    else if (uploadFile === '') {
+        menu1.style.display = 'flex';
+        menu3.style.display = 'none';
+    }
+})
 
 uploadBtn.addEventListener('click', function() {
-    input.click();
-});
+    uploadInput.click();
+})
 
-input.onchange = function() {
-    getFileNameWithExt(event);
-}
+fileCancel.addEventListener('click', function() {
+    uploadInput.value = '';
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    canvasCapture.src = '';
+    canvasCapture.style.display = 'none';
+    fileName.style.display = 'block';
+    var event = new Event('change');
+    uploadForm.dispatchEvent(event);
+})
+
+fileContinue.addEventListener('click', function() {
+    console.log(uploadInput.value);
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    if (uploadFile === undefined) {
+        menu2.style.display = 'none';
+        menu3.style.display = 'none';
+        fileName.innerHTML = 'No file selected.'
+        fileName.style.display = 'block';
+    }
+});
--- app/views/pages/upload.php
+++ app/views/pages/upload.php
@@ -8,24 +8,75 @@ if (!defined('RESTRICTED')) {
 
     <div id="uploadWrapper" class="flex flex-col justify-center shadow bg-white lg:rounded md:rounded slideUp">
         <h1 class="text-3xl text-center mb-4">File upload</h1>
-        <div class="uploadMedia">
-            
+        <div id="uploadPreview" class="uploadMedia">
+            <span id="fileName" style="display: none"></span>
+            <video autoplay="true" id="videoPlayer" style="display: none"></video>
+            <canvas id="canvas" height="" width="" style="display: none"></canvas>
+            <img id="canvasCapture" src="" alt="capture" style="display: none">
         </div>
-        <div class="uploadSelect flex flex-row mt-2">
-            <input id="fileUpload" type='file' hidden/>
+        <form id="uploadForm" action="POST" hidden>
+            <input
+                id="fileUpload"
+                type='file'
+                accept="image/*"
+                enctype="multipart/form-data"
+                hidden
+            />
+            <input
+                id="webcamUpload"
+                type='file'
+                capture="camera"
+                enctype="multipart/form-data"
+                hidden
+            />
+        </form>
+        <div id="menu1" class="uploadSelect slideDown flex flex-row mt-2">
             <button
                 id="manualUpload"
-                class="mr-1 shadow-md"
+                class="mr-1 shadow-md fileButton"
             >
             <img class="uploadIcon" src="<?=URL?>/app/assets/img/resources/upload.svg" alt="upload">
-            Upload
+            Upload file
             </button>
+
             <button
                 id="webCam"
-                class="ml-1 shadow-md"
+                class="ml-1 shadow-md fileButton"
             >
             <img class="webcamIcon" src="<?=URL?>/app/assets/img/resources/webcam.svg" alt="webcam">
-            Webcam
+            Use webcam
+            </button>
+        </div>
+        <div id="menu2" class="uploadSelect slideDown flex-row mt-2">
+            <button
+                id="camCancel"
+                class="mr-1 shadow-md fileCancel"
+            >
+            <img class="webcamIcon" src="<?=URL?>/app/assets/img/resources/webcam.svg" alt="webcam">
+            Cancel
+            </button>
+            <button
+                id="camCapture"
+                class="ml-1 shadow-md fileContinue"
+            >
+            <img class="webcamIcon" src="<?=URL?>/app/assets/img/resources/webcam.svg" alt="webcam">
+            Capture
+            </button>
+        </div>
+        <div id="menu3" class="uploadSelect slideDown flex-row mt-2">
+            <button
+                id="imgCancel"
+                class="mr-1 shadow-md fileCancel"
+            >
+            <img class="uploadIcon" src="<?=URL?>/app/assets/img/resources/upload.svg" alt="upload">
+            Cancel
+            </button>
+            <button
+                id="imgContinue"
+                class="ml-1 shadow-md fileContinue"
+            >
+            <img class="uploadIcon" src="<?=URL?>/app/assets/img/resources/upload.svg" alt="upload">
+            Continue
             </button>
         </div>
 
@@ -40,4 +91,29 @@ if (!defined('RESTRICTED')) {
                 <button id="webCam" class="mx-auto rounded">Use webcam</button>
                 <button id="manualUpload" class="mx-auto rounded">Upload file</button>
             </form>
+
+
+
+
+                    <div class="uploadConfirm">
+            <input
+                id="fileName"
+                type="text"
+                class="form-input rounded"
+                name="filename"
+                placeholder="Login to leave a comment"
+            >
+            <div>
+                <button
+                class="commentLoginButton"
+                >
+                Cancel
+                </button>
+                <button
+                class="commentSignupButton"
+                >
+                Continue
+                </button>
+            </div>
+        </div>
 -->
